e52a6f64092c2815c8c7b83185df8156ad5ae744
yongchu <youngseok.hwang.s7@elms.hokudai.ac.jp>
2025-10-16 18:12:00 +0900

    pets 6

--- pets-mapping.js
+++ pets-mapping.js
@@ -12,7 +12,7 @@ window.PETS.ALIASES = {
   'Mighty Yak':'Mighty Yak','마이티 야크':'Mighty Yak',
   'Unicorn':'Unicorn','유니콘':'Unicorn',
   'Phoenix':'Phoenix','피닉스':'Phoenix',
-  'Poison Lizard':'Poison Lizard','포이즌 리자드':'Poison Lizard',
+  'Poison Lizard':'Poison Lizard','독마뱀':'Poison Lizard',
   'Diggy':'Diggy','디기':'Diggy',
   'Frosty':'Frosty','프로스티':'Frosty',
   'Spirit Fox':'Spirit Fox','스피릿 폭스':'Spirit Fox',
@@ -21,8 +21,8 @@ window.PETS.ALIASES = {
 };
 
 window.PETS.KO = {
-  'L.A.S.S.I':'라씨','Electro Owl':'일렉트로 아울','Mighty Yak':'마이티 야크',
-  'Unicorn':'유니콘','Phoenix':'피닉스','Poison Lizard':'포이즌 리자드',
+  'L.A.S.S.I':'L.A.S.S.I','Electro Owl':'일렉트로 아울','Mighty Yak':'마이티 야크',
+  'Unicorn':'유니콘','Phoenix':'피닉스','Poison Lizard':'독마뱀',
   'Diggy':'디기','Frosty':'프로스티','Spirit Fox':'스피릿 폭스',
   'Angry Jelly':'앵그리 젤리','Sneezy':'스니지',
 };
